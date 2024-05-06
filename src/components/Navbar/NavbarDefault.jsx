9225d129ad1c1c5c74eefb3cf1ffe5869fff562e
harshad <Harshadkajale333@gmail.com>
2024-05-06 16:36:00 +0530

    navbar fixed

--- src/components/Navbar/NavbarDefault.jsx
+++ src/components/Navbar/NavbarDefault.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 function NavbarDefault() {
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(true);
   const [navInput,setNavInput]=useState(false)
 
   const toggleMobileMenu = () => {
