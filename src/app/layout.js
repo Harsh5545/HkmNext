7feb2747feae4d7c0ef1450bc7c7c325f1f3664f
Bhavish <Bhavishm009@gmail.com>
2024-07-23 20:50:24 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,45 @@ export const metadata = {
     description: "Learn animation techniques and principles in our interactive classes. Get hands-on experience and unleash your creativity!",
 };
 
+
+const schemaData = {
+    "@context": "http://schema.org",
+    "@type": "Organization",
+    "legalName": "Your Company Name",
+    "url": "https://example.com",
+    "description": "A brief description of your company.",
+    "logo": "https://example.com/logo.png",
+    "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+91-123-456-7890",
+        "contactType": "customer service",
+        "contactOption": "TollFree",
+        "areaServed": "IN"
+    },
+    "sameAs": [
+        "https://www.facebook.com/yourcompany",
+        "https://twitter.com/yourcompany",
+        "https://www.linkedin.com/company/yourcompany",
+        "https://www.instagram.com/yourcompany"
+    ],
+    "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "123 Street Name",
+        "addressLocality": "City",
+        "addressRegion": "State",
+        "postalCode": "123456",
+        "addressCountry": "IN"
+    }
+};
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
+                <script
+                    type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
+                />
                 <Navbar />
                 <div className=" mx-auto">
                     {children}
