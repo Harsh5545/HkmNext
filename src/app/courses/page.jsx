6e3911e1336cc2061dd99ce82759b44eafc7a028
harshad <Harshadkajale333@gmail.com>
2024-07-24 17:33:25 +0530

    new update

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -8,6 +8,11 @@ export const metadata = {
   alternates: {
     canonical: "/Courses",
   },
+   robots: {
+        index: true,
+        follow: true,
+    },
+
   keywords: `'3D Architectural Design','Harikrushna Multimedia Institue', 'Video Editing', 'Game Design' , '3D Animation', 'Visual Effects', '2D Animation', '3D Jewellery Design', 'Graphics Design' `,
   openGraph: {
     title: "HKM - Courses",
@@ -15,7 +20,17 @@ export const metadata = {
       "Welcome to Harikrushna Multimedia Institute, located in Surat, Gujarat. We take pride in offering a diverse range of creative courses that empower aspiring individuals in the fields of multimedia, animation, and design. Our Location is Harikrushna Multimedia Institute, Shop No-7, Millenium Arcade, Hazira Rd, below You Broadband, opposite Royal Enfield Showroom, Aahura Nagar Society, Adajan Gam, Adajan, Surat, Gujarat 395009",
     keywords: `'3D Architectural Design', 'Video Editing', 'Game Design' , '3D Animation', 'Visual Effects', '2D Animation', '3D Jewellery Design', 'Graphics Design'`,
     images: ["/logo.png", ],
+url: "https://www.harikrushnamultimedia.com/courses",
+        type: "website",
+
   },
+ twitter: {
+        card: 'summary_large_image',
+        title: "Hari Krushna Multimedia",
+        description: "Learn animation techniques and principles in our interactive classes. Get hands-on experience and unleash your creativity!",
+        images: [],
+    }
+
 };
 
 const page = () => {
--- src/app/student-project/page.jsx
+++ src/app/student-project/page.jsx
@@ -9,6 +9,11 @@ export const metadata = {
   alternates: {
     canonical: "/student-project",
   },
+   robots: {
+        index: true,
+        follow: true,
+    },
+
   keywords: `'3D Architectural Design work', 'Surat multimedia school','Multimedia courses in Gujarat','3D architectural design projects', 'Student Animation Project Videos', 'Spider Man Graphic', 'Jewellery Design''Harikrushna Multimedia Institue',  `,
   openGraph: {
     title: "HKM - Student Project",
@@ -16,7 +21,17 @@ export const metadata = {
       "xplore the impressive projects created by the talented students of Harikrishna Multimedia Institute in Surat, Gujarat. Our students excel in 3D architectural design, jewellery design, and animation projects, showcasing creativity and technical skills. View stunning animations, intricate jewellery designs, and more from our comprehensive multimedia courses.",
     keywords: `'3D Architectural Design', 'jewellery work', 'Multimedia courses in Gujarat', 'Student Animation Project Videos', 'Spider Man Graphic', 'Jewellery Design'`,
     images: ["/logo.png", ],
+    url: "https://www.harikrushnamultimedia.com/student-project",
+        type: "website",
+
   },
+ twitter: {
+        card: 'summary_large_image',
+        title: "Hari Krushna Multimedia",
+        description: "Learn animation techniques and principles in our interactive classes. Get hands-on experience and unleash your creativity!",
+        images: [],
+    }
+
 };
 
 const page = () => {
