9909de04b3ef9f496e6aad3a770ee63d37c3cb2b
Bhavish <Bhavishm009@gmail.com>
2024-08-15 01:19:32 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*',
-                    },
-                    {
-                        key: 'Referrer-Policy',
-                        value: 'no-referrer'
-                    },
-                    {
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload',
-                    },
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET, POST, PUT, OPTIONS',
-                    },
-                    {
-                        key: 'X-DNS-Prefetch-Control',
-                        value: 'on'
-                    }
-                ],
-            },
-        ];
-    },
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/(.*)',
+    //             headers: [
+    //                 {
+    //                     key: 'Access-Control-Allow-Origin',
+    //                     value: '*',
+    //                 },
+    //                 {
+    //                     key: 'Referrer-Policy',
+    //                     value: 'no-referrer'
+    //                 },
+    //                 {
+    //                     key: 'Strict-Transport-Security',
+    //                     value: 'max-age=63072000; includeSubDomains; preload',
+    //                 },
+    //                 {
+    //                     key: 'Cache-Control',
+    //                     value: 'public, max-age=31536000, immutable',
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Methods',
+    //                     value: 'GET, POST, PUT, OPTIONS',
+    //                 },
+    //                 {
+    //                     key: 'X-DNS-Prefetch-Control',
+    //                     value: 'on'
+    //                 }
+    //             ],
+    //         },
+    //     ];
+    // },
 };
 
 export default nextConfig;
