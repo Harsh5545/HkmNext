c16bfe0f9af788caf1f3206c50cf7446ac9bb421
Bhavish <Bhavishm009@gmail.com>
2024-08-14 18:09:36 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,41 @@
 const nextConfig = {
     async headers() {
         return [
-          {
-            source: '/(.*)',
-            headers: [
-              {
-                key: 'Cache-Control',
-                value: 'public, max-age=31536000, stale-while-revalidate=60',
-              },
-            ],
-          },
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none';",
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'no-referrer',
+                    },
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
         ];
-      },
+    },
 };
 
 export default nextConfig;
