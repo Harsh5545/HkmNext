72b48aafeb09f98ccce6ee108a611b013aa03ea5
Bhavish <Bhavishm009@gmail.com>
2024-08-15 00:34:37 +0530

    headers added

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,24 @@ const nextConfig = {
                 source: '/(.*)',
                 headers: [
                     {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
                         key: 'Cache-Control',
                         value: 'public, max-age=31536000, immutable',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST, PUT, DELETE, OPTIONS',
+                    },
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on'
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
                     },
                 ],
             },
