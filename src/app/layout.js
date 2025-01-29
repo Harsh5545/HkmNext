44661f5b501ff6e2d34dc44fa4bbb43cac779a12
Harsh <harshadkajale333@gmail.com>
2025-01-30 00:48:25 +0530

    df

--- src/app/layout.js
+++ src/app/layout.js
@@ -80,12 +80,24 @@ export default function RootLayout({ children }) {
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
                 />
+                <Script
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "q1mhol4dau");`,
+                    }}
+                />
+
                 <EnhancedLoader>
-                <Navbar />
-                <div className=" mx-auto">
-                    {children}
-                </div>
-                <Footer /></EnhancedLoader>
+                    <Navbar />
+                    <div className=" mx-auto">
+                        {children}
+                    </div>
+                    <Footer />
+                </EnhancedLoader>
             </body>
         </html>
     );
